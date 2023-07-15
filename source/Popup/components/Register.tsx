59d3aa9ca8f0837ab3193649a27cf40b91c97e64
amiof <arm131313@gmail.com>
2023-07-15 15:58:27 +0330

    add Register ui and some things fixed

--- source/Popup/components/Register.tsx
+++ source/Popup/components/Register.tsx
@@ -2,7 +2,36 @@ import React from 'react'
 import "./scss/Register.scss"
 const Register = () => {
   return (
-    <div className='register'>Register</div>
+
+    <div className="Register">
+      <div id='container'>
+
+        <h2>Register</h2>
+        <div>
+          <div className='Input'>
+            <input type='text' name='username' placeholder="username" />
+          </div>
+
+          <div className='Input'>
+            <input type='text' name='email' placeholder="email" />
+          </div>
+
+          <div className='Input'>
+            <input type='text' name='first_name' placeholder="first_name" />
+          </div>
+          <div className='Input'>
+            <input type='text' name='last_name' placeholder="last_name" />
+          </div>
+          <div className='Input'>
+            <input type='password' name='password' placeholder="password" />
+          </div>
+          <div className='Input'>
+            <input type='text' name='age' placeholder="age" />
+          </div>
+          <button className='RegisterButton' >register</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
