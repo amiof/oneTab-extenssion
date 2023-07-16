9bd079b7d95c0cda3d57883f837b349e9b397482
amiof <arm131313@gmail.com>
2023-07-16 17:25:47 +0330

    prettier change

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -5,27 +5,25 @@ import SideBar from './components/Sidebar';
 import './styles.scss';
 import Register from './components/Register';
 
-
 const Popup: React.FC = () => {
-  const [Click, setClick] = React.useState<string>("notClicked")
-  console.log(Click)
+  const [Click, setClick] = React.useState<string>('notClicked');
+  console.log(Click);
   const urlProps = {
-    setClick: setClick
-
-  }
+    setClick,
+  };
   return (
-    <div className='container'>
-      <div className='title'>
-        <h2 >ONE-Tab</h2>
-        <div className='divider'></div>
+    <div className="container">
+      <div className="title">
+        <h2>ONE-Tab</h2>
+        <div className="divider" />
       </div>
-      <div className='popUp'>
+      <div className="popUp">
         <SideBar {...urlProps} />
-      </div >
-      <div className='content'>
-        {Click == "login" ? <Login></Login> : null}
-        {Click == "register" ? <Register></Register>: null}
-        {Click == "urls"||Click=="notClicked" ? <AllUrls></AllUrls> : null}
+      </div>
+      <div className="content">
+        {Click == 'login' ? <Login /> : null}
+        {Click == 'register' ? <Register /> : null}
+        {Click == 'urls' || Click == 'notClicked' ? <AllUrls /> : null}
       </div>
     </div>
   );
