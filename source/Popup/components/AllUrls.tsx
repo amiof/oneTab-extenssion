5c4145ae2b42dc28ba02c692566b01b4341cd270
amiof <arm131313@gmail.com>
2023-07-16 17:28:07 +0330

    get url from server and show that and add urlCard for latter devlop

--- source/Popup/components/AllUrls.tsx
+++ source/Popup/components/AllUrls.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
-import "./scss/allUrls.scss"
-const AllUrls = () => {
-  return (
-    <div className='allurls-contaier'>allUrls</div>
-  )
-}
+import React, {useState, useEffect} from 'react';
+import './scss/allUrls.scss';
 
-export default AllUrls
+export type Tuser = {
+  id: string;
+  email: string;
+  userName: string;
+  first_name: string;
+  last_name: string;
+  password: string;
+  age: number;
+  JwtToken: string;
+};
+
+type Turls = {
+  id: string;
+  url: string;
+  title: string;
+  user: Tuser;
+  createAt: string;
+};
+const AllUrls = () => {
+  const [urls, setUrls] = useState<Turls[]>([]);
+
+  function getDataUrls() {
+    return fetch('http://localhost:3000/urls')
+      .then((res) => res.json())
+      .then((data) => {
+        setUrls(data);
+      });
+  }
+
+  useEffect(() => {
+    getDataUrls();
+  }, []);
+
+  // console.log("outSide", urls);
+
+  return (
+    <>
+      {urls.map((url) => (
+        <p key={url.id}>{url.title}</p>
+      ))}
+    </>
+  );
+};
+
+export default AllUrls;
---
+++ source/Popup/components/UrlCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UrlCard = () => {
+  return <div>UrlCard</div>;
+};
+
+export default UrlCard;
