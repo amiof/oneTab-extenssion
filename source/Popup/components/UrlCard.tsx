d566bedbd43320ffce3d85cdc1114b98df922242
amiof <arm131313@gmail.com>
2023-07-26 12:59:27 +0330

    add react icons for urlcard and add some icon

--- source/Popup/components/UrlCard.tsx
+++ source/Popup/components/UrlCard.tsx
@@ -1,28 +1,45 @@
 import React, { useState } from "react";
 import { Turls } from "./AllUrls";
 import "./scss/urlCard.scss";
-
+import { BiShowAlt } from "react-icons/bi";
+import { AiFillDelete } from "react-icons/ai";
+import { AiOutlineStar } from "react-icons/ai";
 const UrlCard = ({ id, url, title }: Turls) => {
   const [more, setMore] = useState<boolean>(false);
   console.log(id);
   return (
-    <div>
+    <div id="record">
       <div id="Container">
         <div className="content">
           <div className="tit">
             <p>{title}</p>
             <p className={!more ? "hidden" : ""}>{url}</p>
           </div>
-          <div>
-            <button className="more"
-              onClick={() => {
-                setMore(!more);
-              }}
-            >
-              x
-            </button>
-          </div>
         </div>
+      </div>
+      <div id="ButtonContainer">
+        <BiShowAlt
+          className="icons"
+          color="yellow"
+          onClick={() => {
+            setMore(!more);
+          }}
+        ></BiShowAlt>
+
+        <AiFillDelete
+          className="icons"
+          color="red"
+          onClick={() => {
+            setMore(!more);
+          }}
+        />
+        <AiOutlineStar
+          className="icons"
+          color="yellow"
+          onClick={() => {
+            setMore(!more);
+          }}
+        />
       </div>
     </div>
   );
