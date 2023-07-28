e98b4fb09f14f75d30f850071acb859933e24434
amiof <arm131313@gmail.com>
2023-07-28 16:35:53 +0330

    add accourdion card to cardurl

--- source/Popup/components/UrlCard.tsx
+++ source/Popup/components/UrlCard.tsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { Turls } from "./AllUrls";
 import "./scss/urlCard.scss";
-import { BiShowAlt } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
 import { AiOutlineStar } from "react-icons/ai";
-import { browser } from "webextension-polyfill-ts";
-
-
+import { BiShowAlt } from "react-icons/bi";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  CardActionArea,
+  CardActions,
+} from "@mui/material";
 
 //FIXME: favicon is not show in card url
 //
@@ -18,69 +23,45 @@ type partialTurls = Partial<Turls> & {
   favIconUrl?: string;
 };
 const UrlCard = ({ id, url, title }: partialTurls) => {
-  const [more, setMore] = useState<boolean>(false);
   console.log(id);
-  const favIconUrls = url + "/favicon.ico";
-  const [image, setImage] = useState<string | undefined>(undefined);
 
   // Use the useEffect() hook to fetch the image and create a URL for it
-  React.useEffect(() => {
-    fetch(favIconUrls)
-      .then((response) => response.blob())
-      .then((blob) => {
-        setImage(URL.createObjectURL(blob));
-      })
-      .catch((error) => {
-        console.error(`Error fetching favicon for ${url}: ${error}`);
-      });
-  }, [favIconUrls]);
 
   return (
     <div id="record">
-      {image && <img src={image}  alt="Favicon" style={{ width: "32px" }}/>}
-      <div id="Container">
-        <div className="content">
-          <div className="tit">
-            {console.log(url + "/favicon.ico")}
-            <p>{title}</p>
-            <p className={!more ? "hidden" : ""}>{url}</p>
-          </div>
-          <p
-            onClick={(): Promise<void> => {
-              return browser.tabs.query({ pinned: false }).then((tabs) => {
-                // const activeTab = tabs[0];
-                // return browser.tabs.create({ url: activeTab.url });
-                console.log(tabs);
-              });
-            }}
-          >
-            open
-          </p>
-        </div>
-      </div>
+      <Accordion
+        sx={{
+          width: "70%",
+          marginY: "5px",
+          background: "#201e1e",
+          color: "white",
+          border: "1px solid gray",
+        }}
+      >
+        <AccordionSummary>{title}</AccordionSummary>
+        <AccordionDetails>{url}</AccordionDetails>
+        <CardActionArea>
+          <CardActions>
+            <Button variant="outlined" color="error">
+              <AiFillDelete className="icons" color="red" onClick={() => { }} />
+            </Button>
+            <Button variant="outlined" color="primary">
+              <AiOutlineStar
+                className="icons"
+                color="yellow"
+                onClick={() => { }}
+              />
+            </Button>
+            <Button variant="outlined" color="secondary">
+              <BiShowAlt className="icons" color="green" onClick={() => { }} />
+            </Button>
+          </CardActions>
+        </CardActionArea>
+      </Accordion>
       <div id="ButtonContainer">
-        <BiShowAlt
-          className="icons"
-          color="yellow"
-          onClick={() => {
-            setMore(!more);
-          }}
-        ></BiShowAlt>
-
-        <AiFillDelete
-          className="icons"
-          color="red"
-          onClick={() => {
-            setMore(!more);
-          }}
-        />
-        <AiOutlineStar
-          className="icons"
-          color="yellow"
-          onClick={() => {
-            setMore(!more);
-          }}
-        />
+        <BiShowAlt className="icons" color="green" onClick={() => { }} />
+        <AiFillDelete className="icons" color="red" onClick={() => { }} />
+        <AiOutlineStar className="icons" color="yellow" onClick={() => { }} />
       </div>
     </div>
   );
