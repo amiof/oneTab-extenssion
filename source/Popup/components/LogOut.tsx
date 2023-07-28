0d02f308badd735e91752c0966ca5f1a053877d6
amiof <arm131313@gmail.com>
2023-07-28 11:17:07 +0330

    added container to logout page

--- source/Popup/components/LogOut.tsx
+++ source/Popup/components/LogOut.tsx
@@ -1,36 +1,40 @@
+import { Box, Button, Card, CardActions, CardContent, CardHeader, makeStyles, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 import React, { useEffect, useState } from "react";
 import { browser } from "webextension-polyfill-ts";
 import { Tuser } from "./AllUrls";
 
 const LogOut = () => {
-  // let user: Tuser = {
-  //   id: "",
-  //   first_name: "",
-  //   age: 0,
-  //   email: "",
-  //   JwtToken: "",
-  //   password: "",
-  //   userName: "",
-  //   last_name: "",
-  // };
-  //
   const [userData, setUserData] = useState<Tuser>();
-  useEffect(()=>{
+  useEffect(() => {
     const storage = browser.storage.local.get("userData");
-    storage.then((data) =>setUserData(data.userData) );
-  },[])
-  const clickHandler=()=>{
+    storage.then((data) => setUserData(data.userData));
+  }, []);
 
+  const clickHandler = () => {
     browser.storage.local.remove("userData");
+  };
 
-  }
-  console.log("logout", userData);
 
-  return <div>`hi `{userData?.first_name}
+  return (
+    <Container maxWidth="xs" sx={{ marginTop: "60px" }}>
+      <Box>
+        <Card variant="outlined">
+          <CardHeader title={`hi ${userData?.first_name}`} sx={{ background: "#171616", color: "white" }} />
 
-<button onClick={clickHandler} >logeout</button>
-  </div>;
+          <CardContent style={{ background: "#171616", color: "white" }}>
+            <Typography variant="h5">`your urls is  `{userData?.first_name}</Typography>
+          </CardContent>
 
+          <CardActions>
+            <Button variant="contained" size="small" sx={{ ":hover": { backgroundColor: "red" } }} onClick={clickHandler}>
+              logout
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    </Container>
+  );
 };
 
 export default LogOut;
