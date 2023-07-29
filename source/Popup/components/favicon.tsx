da538f9aff5b2d417b5e8006fb4733a1d089e909
amiof <arm131313@gmail.com>
2023-07-29 08:25:58 +0330

    add url for favicon

--- source/Popup/components/favicon.tsx
+++ source/Popup/components/favicon.tsx
@@ -3,14 +3,15 @@ import { useState, useEffect } from "react";
 interface Props {
   urls:{url:string,favIconUrls:string|undefined}  ;
 }
-
+//NOTE: you can use this urls :https://s2.googleusercontent.com/s2/favicons?domain_url=https://www.stackoverflow.com
+//or https://s2.googleusercontent.com/s2/favicons?domain=www.stackoverflow.com
 const Favicon: FC<Props> = ({ urls:{url} }) => {
   const [faviconUrl, setFaviconUrl] = useState<string | null>(null);
 
   useEffect(() => {
     async function fetchFavicon() {
       try {
-        const faviconUrl = new URL(`https://s2.googleusercontent.com/s2/favicons?domain_url=${url}`);
+        const faviconUrl = new URL(`https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=16`);
         const response = await fetch(faviconUrl.toString());
         const blob = await response.blob();
         const imageUrl = URL.createObjectURL(blob);
