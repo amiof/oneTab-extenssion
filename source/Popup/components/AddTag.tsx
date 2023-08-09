fe4f0d6b0cc580e261182e9d07ca15866c4aa797
amiof <arm131313@gmail.com>
2023-08-09 09:05:52 +0330

    add warp in tabbar and add fucntion for remove tag withe ui

--- source/Popup/components/AddTag.tsx
+++ source/Popup/components/AddTag.tsx
@@ -7,6 +7,7 @@ import TagCard from "./tagCard";
 const AddTag = () => {
   const [tagName, setTagName] = useState<string>("");
   const [tags, setTags] = useState<tagRes[]>([]);
+  const [remvoed, setRemoved] = useState<string>("")
   type tagRes = {
     id: string;
     TagName: string;
@@ -22,13 +23,25 @@ const AddTag = () => {
       .then((data) => setTags(data))
       .then(() => console.log(tags))
   };
+
+
   const addTag = () => {
-    console.log("clicke to add Tag")
+    fetch("http://localhost:3000/tag", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        TagName: tagName,
+        email: "ali7@gmai.com",
+      })
+
+    }).then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
     getTags();
   };
   useEffect(() => {
     getTags();
-  }, []);
+  }, [remvoed]);
   return (
     <div style={{ width: "100%", marginTop: "50px" }}>
       <Toolbar>
@@ -56,11 +69,11 @@ const AddTag = () => {
             </Button>
           }
           inputProps={{ "aria-label": "tag", color: "gray" }}
-          placeholder="type your tag"
+          placeholder="type your tagName"
         />
       </Toolbar>
-      <Box sx={{ display: "flex", flex: "auto" }}>
-        {tags.map((tag) => <TagCard key={tag.id} TagName={tag.TagName} />)}
+      <Box sx={{ display: "flex", flex: "auto",flexWrap:"wrap" }}>
+        {tags.map((tag) => <TagCard key={tag.id} TagName={tag.TagName} TagId={tag.id} setRemoved={setRemoved}/>)}
 
       </Box>
     </div>
--- source/Popup/components/tagCard.tsx
+++ source/Popup/components/tagCard.tsx
@@ -3,16 +3,25 @@ import React from 'react'
 
 import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
 
+const deleteTag = (tagId: string) => {
+  fetch(`http://localhost:3000/tag/RemoveTag/${tagId}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: "ali7@gmai.com" }),
+  })
+}
 type props = {
   TagName: string
+  TagId: string
+  setRemoved: React.Dispatch<React.SetStateAction<string>>
 }
 const TagCard = (Props: props) => {
-  const { TagName } = Props
+  const { TagName, TagId, setRemoved } = Props
   return (
     <Card sx={{ background: "#171616", width: "fit-content", borderRadius: "20px", height: "fit-content", border: "1px solid gray", margin: "15px", ":hover": { border: "1px solid red" } }}>
       <CardContent sx={{ color: "white" }}>
         <Box sx={{ color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <IconButton>
+          <IconButton onClick={() => { deleteTag(TagId); setRemoved(TagId) }}>
             <HighlightOffOutlinedIcon sx={{ color: "white" }} />
           </IconButton>
           <Typography fontSize="17px">
