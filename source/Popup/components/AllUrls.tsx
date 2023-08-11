411344eaacd842eb5edd37036fbeb32fc5e0b1ef
amiof <arm131313@gmail.com>
2023-08-11 17:55:53 +0330

    add header to urls now it need to fix some things

--- source/Popup/components/AllUrls.tsx
+++ source/Popup/components/AllUrls.tsx
@@ -1,5 +1,5 @@
-import { CircularProgress } from "@mui/material";
-import { Box } from "@mui/system";
+import { CircularProgress, Collapse, List, ListItemButton, ListItemText, ListSubheader } from "@mui/material";
+import { Box } from "@mui/material";
 import React, { useState, useEffect } from "react";
 import "./scss/allUrls.scss";
 import UrlCard from "./UrlCard";
@@ -42,8 +42,10 @@ export type Turls = {
 };
 const AllUrls = () => {
   const [urls, setUrls] = useState<Turls[]>([]);
+  const [colaps, setColaps] = useState<boolean>(false)
+  const [headers, setHeaders] = useState<Theader[]>([])
 
-  function getDataUrls() {
+  function getDataUrls(): Promise<void> {
     return fetch("http://localhost:3000/urls")
       .then((res) => res.json())
       .then((data) => {
@@ -51,28 +53,79 @@ const AllUrls = () => {
       });
   }
 
+  function getHeader(): Promise<void> {
+    return fetch("http://localhost:3000/headers/userHeader", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ userEmail: "ali7@gmai.com" }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setHeaders(data);
+      });
+  }
+
   useEffect(() => {
     getDataUrls();
+    getHeader()
   }, []);
 
   console.log("outSide", urls[0]);
 
   return (
     <>
+      <List
+        sx={{
+          width: '100%',
+          background: "#171616",
+        }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader" sx={{ color: "white", background: "#171616" }}>
+            Nested List Items
+          </ListSubheader>
+        }
+      >
+        {headers.map((header) => <>
+          <ListItemButton onClick={() => { setColaps(!colaps) }}>
+            <ListItemText primary={header.headerName} />
+          </ListItemButton>
+          <Collapse in={colaps} timeout="auto">
+            {urls[0]?.id ?
+              urls.map((url) => <UrlCard key={url.id} {...url}></UrlCard>) : (
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginY: "30%",
+                  }}>
+                  <CircularProgress />
+                </Box>
+              )}
+          </Collapse>
+        </>
+        )}
+
+
+      </List>
       {urls[0]?.id ? (
-        urls.map((url) => <UrlCard key={url.id} {...url}></UrlCard>)
-      ) : (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            marginY: "30%",
-          }}
-        >
-          <CircularProgress />
-        </Box>
-      )}
+        // urls.map((url) => <UrlCard key={url.id} {...url}></UrlCard>)
+        <div></div>
+      )
+        : (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              marginY: "30%",
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        )}
     </>
   );
 };
